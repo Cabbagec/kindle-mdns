577e4ea9765ce35e0c65ba6904517189072b6c3c
cabbagec <root@everdream.xyz>
2024-12-06 18:21:43 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use clap::Parser;
+use mdns_sd::{IfKind, ServiceDaemon, ServiceInfo};
+use std::process::exit;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value = "_my-hello._tcp")]
+    service_type: Option<String>,
+    #[arg(short, long, default_value = "instance1")]
+    instance_name: Option<String>,
+    host_name: String,
+    #[arg(long = "disable-ipv6", default_value_t = false)]
+    disable_ipv6: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    let Ok(service) = ServiceInfo::new(
+        format!("{}.local.", args.service_type.unwrap()).as_str(),
+        args.instance_name.unwrap().as_str(),
+        format!("{}.local.", args.host_name).as_str(),
+        "",
+        0,
+        None,
+    ) else {
+        eprintln!("Failed to create ServiceInfo");
+        exit(1);
+    };
+    let service = service.enable_addr_auto();
+    let Ok(mdns) = ServiceDaemon::new() else {
+        eprintln!("Failed to create ServiceDaemon");
+        exit(1);
+    };
+    if args.disable_ipv6 {
+        let _ = mdns.disable_interface(IfKind::IPv6);
+    }
+
+    if let Err(err) = mdns.register(service) {
+        eprintln!("Failed to register service: {}", err);
+        exit(1);
+    }
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(10));
+    }
+}
